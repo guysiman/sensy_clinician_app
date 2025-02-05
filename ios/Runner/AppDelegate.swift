625879defb10fd4f6f2fd668500a0a5f7357fa48
Abhay Yer <abhay.iyer7@gmail.com>
2025-02-05 13:11:10 -0600

    firebase integration, still some issues with running the build, will resolve

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,29 @@
-import Flutter
 import UIKit
+import Flutter
+import FirebaseCore  // ✅ Import Firebase
 
-@main
+
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()  // ✅ Initialize Firebase
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+@main
+struct YourApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
 }
