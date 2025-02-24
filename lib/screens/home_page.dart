2f886c8ec6eca8bb226a3f8568ecbd3c9ea41672
jiejen <jiezhang0123@gmail.com>
2025-02-24 11:19:56 -0600

    add firebase auth

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import '../services/auth.dart';
+
 import 'dashboard_page.dart';
 import 'patient_page.dart';
 
@@ -10,6 +14,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final User? user = Auth().currentUser!;
+
+  Future<void> signOut() async {
+    await Auth().signOut();
+  }
+
   int _selectedIndex = 0;
 
   final List<Widget> _pages = [
@@ -29,46 +39,59 @@ class _HomePageState extends State<HomePage> {
       body: Row(
         children: [
           NavigationRail(
-              selectedIndex: _selectedIndex,
-              onDestinationSelected: _onItemTapped,
-              labelType: NavigationRailLabelType.all,
-              backgroundColor: Theme.of(context).colorScheme.primary,
-              indicatorColor: Color(0xFF61838D),
-              
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(
-                    _selectedIndex == 0 ? Icons.dashboard : Icons.dashboard_outlined,
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: _onItemTapped,
+            labelType: NavigationRailLabelType.all,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            indicatorColor: Color(0xFF61838D),
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(
+                  _selectedIndex == 0
+                      ? Icons.dashboard
+                      : Icons.dashboard_outlined,
+                  color: Colors.white,
+                  size: 40,
+                ),
+                label: Text(
+                  'Dashboard',
+                  style: TextStyle(
                     color: Colors.white,
-                    size: 40,
-                  ),
-                  label: 
-                  Text(
-                    'Dashboard',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: _selectedIndex == 0 ? FontWeight.bold : FontWeight.normal,
-                    ),
+                    fontWeight: _selectedIndex == 0
+                        ? FontWeight.bold
+                        : FontWeight.normal,
                   ),
                 ),
-                NavigationRailDestination(
-                  icon: Icon(
-                    _selectedIndex == 1 ? Icons.people : Icons.people_outlined,
+              ),
+              NavigationRailDestination(
+                icon: Icon(
+                  _selectedIndex == 1 ? Icons.people : Icons.people_outlined,
+                  color: Colors.white,
+                  size: 40,
+                ),
+                label: Text(
+                  'Patients',
+                  style: TextStyle(
                     color: Colors.white,
-                    size: 40,
-                  ),
-                  label: Text(
-                    'Patients',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: _selectedIndex == 1 ? FontWeight.bold : FontWeight.normal,
-                    ),
+                    fontWeight: _selectedIndex == 1
+                        ? FontWeight.bold
+                        : FontWeight.normal,
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
           Expanded(
             child: _pages[_selectedIndex],
+          ),
+          Container(
+            alignment: Alignment.bottomRight,
+            child: ElevatedButton(
+              child: const Text("Logout"),
+              onPressed: () {
+                signOut();
+              },
+            ),
           ),
         ],
       ),
